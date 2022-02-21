58f74c7d4294ff4a26032f26dd2b589a117bf34f
jorgercoder <jorgeretch@hotmail.com>
2022-02-21 07:15:35 -0500

    added ui

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,39 @@
-//create random variable rock papaer scissor of computer
-//create array of options and use random choose
 let options= ['Rock', 'Paper', 'Scissors']
 function ComputerPlay(){
     let computerSelection = options[Math.floor(Math.random()*3)];
     return computerSelection;
 }
-//create variable from user
+let winsPc=0;
+let winsPlayer=0;
 
-
-function playRound(computerSelection , player){
-
+function playRound(event){
+    let computerSelection =ComputerPlay() ;
+    let player = event.target.innerText;
+    let tie =`${player} vs ${computerSelection} is Tie`;
+    let win =`${player} beats ${computerSelection}`;
+    let loose =`${player} losses to ${computerSelection}`;
+    
     player =CapitalizeAll(player);
-    if(computerSelection === player)return 'tie';
+    if(winsPc === 5 || winsPlayer === 5){
+        winsPc=0;
+        winsPlayer=0;
+    }
+    
+    if(computerSelection === player)return `${tie} = score: pc-${winsPc} player-${winsPlayer}`;
     else if (computerSelection === 'Rock'     && player === 'Paper'
           || computerSelection === 'Paper'    && player === 'Scissors'
           || computerSelection === 'Scissors' && player === 'Rock'){
-            return `you win! ${player} beats ${computerSelection}`
+
+              if(winsPlayer === 4) return `${win} = score: pc-${winsPc} player-${++winsPlayer} Player wins!!`;
+              else return `${win} = score: pc-${winsPc} player-${++winsPlayer}`
+            
         }
     else if(computerSelection === 'Rock' && player === 'Scissors'
          || computerSelection === 'Paper'    && player === 'Rock'
           || computerSelection === 'Scissors' && player === 'Paper'){
-            return `you loose :( ${computerSelection} beats ${player}`
-        } 
+              if(winsPc === 4)return `${loose} = score: pc-${++winsPc} player-${winsPlayer} pc wins:(`;
+              else return `${loose} = score: pc-${++winsPc} player-${winsPlayer} `;            
+        }         
 }
 
 function CapitalizeAll(option){
@@ -45,10 +57,14 @@ function CapitalizeAll(option){
     return capitalized
 }
 
-for(let i=0; i<5;i++)
-{
-    let player2 =prompt(`enter options from: ${options}`)
-    let computerSelection=ComputerPlay();
-    console.log(`computer vs player: ${computerSelection} vs ${CapitalizeAll(player2)} = ${playRound(computerSelection,player2)}`);
-}
+const output= document.querySelector('.result');
+const result =document.createElement('div');
+const count2 =document.createElement('div');
 
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click',()=> result.innerText=(playRound(event)));
+});
+
+output.appendChild(result);
